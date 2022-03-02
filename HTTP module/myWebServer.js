7ff61c859f41030e19bbe8dc1f230bc01601f787
mohamedtamer0 <mt4452242@gmail.com>
2022-03-02 21:29:47 +0200

    update

--- HTTP module/myWebServer.js
+++ HTTP module/myWebServer.js
@@ -4,15 +4,32 @@ var HTTPServer = HTTPModule.createServer();
 HTTPServer.on("request", (request, response) => {
   console.log("Request received");
   var url = request.url;
+  console.log(url);
+  console.log(request.method);
+  var products = [
+    { name: "mohamed", age: 25 },
+    { name: "ahmed", age: 30 },
+  ];
   switch (url) {
+    case "/":
+      response.writeHead(302, {
+        Location: "/home",
+      });
+      break;
     case "/home":
+      response.writeHead(200);
       response.write("Hello Clint This Home Page");
       break;
     case "/about":
+      response.writeHead(200);
       response.write("Hello Clint This is about Page");
       break;
     case "/contact":
+      response.writeHead(200);
       response.write("Hello Clint This is contact Page");
+      break;
+    case "/products":
+      response.write(JSON.stringify(products));
       break;
     default:
       response.writeHead(404);
