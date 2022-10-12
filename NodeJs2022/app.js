1f3359a7697733be9ed65df6102accf4296c86f1
mohamed.tamer <mt4452242@gmail.com>
2022-10-12 16:17:41 +0200

    update

--- NodeJs2022/app.js
+++ NodeJs2022/app.js
@@ -3,8 +3,20 @@ const express = require('express');
 //express app
 const app = express();
 
+//express app
+app.set('view engine', 'ejs');
+
 //listen for request
 app.listen(3000);
+
 app.get('/', (req, res) => {
-    res.send('<p>home page</p>');
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
