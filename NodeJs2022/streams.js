19f9a3dbcaf46cf3ce741add7b76b2c3da543ece
mohamed.tamer <mt4452242@gmail.com>
2022-10-12 08:50:54 +0200

    update

--- NodeJs2022/streams.js
+++ NodeJs2022/streams.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt');
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+// readStream.on('data', (chunk) => {
+//     console.log('----New Chunk----');
+//     console.log(chunk.toString());
+// });
+
 
 readStream.on('data', (chunk) => {
     console.log('----New Chunk----');
     console.log(chunk.toString());
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
 });
